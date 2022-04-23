2dbae8ec40ecbe4cdcf1060f71b7cabc1c61900b
o0pmitev <pmitev.dev@gmail.com>
2022-04-23 05:29:55 +0200

    feature: :tada: Finish the palindrome checker function

--- palindromes.js
+++ palindromes.js
@@ -1,3 +1,18 @@
 module.exports = (str) => {
-  return [str];
-}
+  
+  let lowerCaseNoSymbolsStr = str
+    .toLowerCase()
+    .replace(/[^\w\s]|_/g, "")
+    .replace(/\s+/g, "");
+
+  if (
+    lowerCaseNoSymbolsStr
+      .split("")
+      .reverse()
+      .join("") !== lowerCaseNoSymbolsStr
+  ) {
+    return [];
+  }
+
+  return [lowerCaseNoSymbolsStr];
+};
--- tests/palindromes.test.js
+++ tests/palindromes.test.js
@@ -3,6 +3,26 @@ const palindromes = require("../palindromes");
 describe("palindromes()", () => {
   it("correctly identifies one-word palindromes", () => {
     expect(palindromes("madam")).toEqual(["madam"]);
-    expect(palindromes("racecar")).toEuqal(["racecar"]);
+    expect(palindromes("racecar")).toEqual(["racecar"]);
+  });
+
+  it("returns an empty array when a given no palindromes", () => {
+    expect(palindromes("tic tac toe")).toEqual([]);
+  });
+
+  it("ignores casing", () => {
+    expect(palindromes("WoW")).toEqual(["wow"]);
+  });
+
+  it("ignores punctuation", () => {
+    expect(palindromes("yo, banana boy!")).toEqual(["yobananaboy"]);
+  });
+  it("ignores whitespace at beggining and at the end", () => {
+    expect(palindromes(" yo, banana boy! ")).toEqual(["yobananaboy"]);
+  });
+  it("detect multi-word palindromes", () => {
+    expect(palindromes("A man, a plan, a canal, Panama")).toEqual(
+      ["amanaplanacanalpanama"]
+    );
   });
 });
